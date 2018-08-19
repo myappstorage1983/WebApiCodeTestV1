0e13ffce37703516b31bac7cf3204c3b19a6199b
DESKTOP-SVRU7VV\leela <myappstore1983@gmail.com>
2018-08-19 23:07:42 +1000

    cz

--- TreeViewTesting/TreeViewControllerTest.cs
+++ TreeViewTesting/TreeViewControllerTest.cs
@@ -28,47 +28,7 @@ namespace TreeViewTesting
             _controller = new TreeViewController(_folderServiceMock.Object, _testCaseServiceMock.Object);
         }
 
-        [TestMethod]
-        public async Task GetTestCasesInsideFolderTest_Success()
-        {
-            //Arrange
-            _testCaseServiceMock.Setup(service => service.GetTestCases(It.IsAny<int>()))
-                .ReturnsAsync(getMockTestCaseDtoList());
 
-            // Act
-            var response = await _controller.GetTestCasesInsideFolder(1);
-
-            //var actionResult = (ActionResult<List<TestCaseDto>>)response.Result;
-            //var result = (OkObjectResult)((ActionResult<List<TestCaseDto>>)response.Result).Result;
-
-            ////Assert
-
-            //Assert.IsNotNull(response);
-            //_testCaseServiceMock.Verify(t => t.GetTestCases(1), Times.Once);
-            //Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.OK);
-
-            var okResult = response.Result as OkObjectResult;
-            //Assert
-            Assert.IsInstanceOfType(okResult.Value, typeof(List<TestCaseDto>));
-        }
-
-        [TestMethod]
-        public async Task GetTestCasesInsideFolderTest_FailureNotFound()
-        {
-            //Arrange
-            _testCaseServiceMock.Setup(service => service.GetTestCases(It.IsAny<int>()))
-                .ReturnsAsync(new List<TestCaseDto>());
-
-            // ACT
-            var response = await _controller.GetTestCasesInsideFolder(101);
-            var actionResult = (ActionResult<List<TestCaseDto>>)response.Result;
-            var result = (NotFoundResult)((ActionResult)response.Result);
-
-            //Assert
-            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.NotFound);
-        }
-
-      
         private static List<TestCaseDto> getMockTestCaseDtoList()
         {
             return new List<TestCaseDto>() {
@@ -86,5 +46,138 @@ namespace TreeViewTesting
                         }
                 };
         }
+
+        [TestMethod]
+        public async Task GetTestCasesInsideFolderTest_Success_OkResponse()
+        {
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.GetTestCases(It.IsAny<int>()))
+                .ReturnsAsync(getMockTestCaseDtoList());
+
+            // Act
+            var response = await _controller.GetTestCasesInsideFolder(1);
+
+            var actionResult = (ActionResult<List<TestCaseDto>>)response.Result;
+            var result = (OkObjectResult)((ActionResult<List<TestCaseDto>>)response.Result).Result;
+
+            //Assert
+
+            Assert.IsNotNull(response);
+            _testCaseServiceMock.Verify(t => t.GetTestCases(1), Times.Once);
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.OK);
+
+            //var okResult = response.Result as OkObjectResult;
+            Assert.IsInstanceOfType(result.Value, typeof(List<TestCaseDto>));
+        }
+
+        [TestMethod]
+        public async Task GetTestCasesInsideFolderTest_Failure_NotFound()
+        {
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.GetTestCases(It.IsAny<int>()))
+                .ReturnsAsync(new List<TestCaseDto>());
+
+            // ACT
+            var response = await _controller.GetTestCasesInsideFolder(101);
+            var actionResult = (ActionResult<List<TestCaseDto>>)response.Result;
+            var result = (NotFoundResult)((ActionResult)response.Result);
+
+            //Assert
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public async Task AddTestCase_Success_CreatedResponse()
+        {
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.AddTestCase(It.IsAny<TestCaseDto>()))
+                .ReturnsAsync(true);
+
+            // ACT
+            var response = (IActionResult)await _controller.AddTestCase(new TestCaseDto()
+            {
+                TestCaseId = 12,
+                Name = "TestCase12",
+                StepCount = 12,
+                FolderId = 1
+            });
+
+            var result = (StatusCodeResult)response;
+
+            //Assert
+            Assert.IsNotNull(result);
+            _testCaseServiceMock.Verify(t => t.AddTestCase(It.IsAny<TestCaseDto>()), Times.Once);
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.Created);
+        }
+
+        [TestMethod]
+        public async Task AddTestCase_Failure_BadRequest()
+        {
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.AddTestCase(It.IsAny<TestCaseDto>()))
+                .ReturnsAsync(false);
+
+            // ACT
+            var response = (IActionResult)await _controller.AddTestCase(new TestCaseDto()
+            {
+                TestCaseId = 12,
+                Name = "TestCase12",
+                StepCount = 12,
+                FolderId = 100000023
+            });
+            var result = (StatusCodeResult)response;
+
+            //Assert
+            Assert.IsNotNull(result);
+            _testCaseServiceMock.Verify(t => t.AddTestCase(It.IsAny<TestCaseDto>()), Times.Once);
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.BadRequest);
+        }
+
+        [TestMethod]
+        public async Task DeleteTestCase_Success_AcceptedResponse()
+        {
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.DeleteTestCase(It.IsAny<TestCaseDto>()))
+                .Returns(Task.CompletedTask);
+            _testCaseServiceMock.Setup(service => service.GetTestCase(It.IsAny<int>()))
+                .ReturnsAsync(new TestCaseDto()
+                {
+                    TestCaseId = 12,
+                    Name = "TestCase12",
+                    StepCount = 12,
+                    FolderId = 100000023
+                });
+
+            // ACT
+            var response = (IActionResult)await _controller.DeleteTestCase(12);
+            var result = (StatusCodeResult)response;
+
+            //Assert
+            Assert.IsNotNull(result);
+            _testCaseServiceMock.Verify(t => t.DeleteTestCase(It.IsAny<TestCaseDto>()), Times.Once);
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.Accepted);
+        }
+
+        [TestMethod]
+        public async Task DeleteTestCase_Failure_NotFoundResponse()
+        {
+            TestCaseDto returnObj = null;
+            //Arrange
+            _testCaseServiceMock.Setup(service => service.GetTestCase(It.IsAny<int>()))
+                .ReturnsAsync(returnObj);
+
+            // ACT
+            var response = (IActionResult)await _controller.DeleteTestCase(12);
+            var result = (StatusCodeResult)response;
+
+            //Assert
+            Assert.IsNotNull(result);
+            _testCaseServiceMock.Verify(t => t.GetTestCase(It.IsAny<int>()), Times.Once);
+            _testCaseServiceMock.Verify(t => t.DeleteTestCase(It.IsAny<TestCaseDto>()), Times.Never);
+            Assert.IsTrue(result.StatusCode == (int)HttpStatusCode.NotFound);
+        }
+
+
+
     }
 }
